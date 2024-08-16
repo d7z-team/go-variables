286a92f5ecd6097717874a3cd549224f621b7f3c
年幼涩批 <explodingfkl@gmail.com>
2024-08-16 17:11:16 +0800

    移动 properties 到独立位置

---
+++ variable_properties.go
@@ -0,0 +1,30 @@
+package variables
+
+import (
+	"strings"
+
+	"github.com/pkg/errors"
+)
+
+func (p *Variables) FromProperties(src string) error {
+	lines := strings.Split(src, "\n")
+	for i := 0; i < len(lines); i++ {
+		line := lines[i]
+		if strings.TrimSpace(line) == "" {
+			continue
+		}
+		key, value, found := strings.Cut(line, "=")
+		if !found {
+			return errors.Errorf("properties 格式错误，位于 %d 行", i)
+		}
+		for strings.HasSuffix(value, "\\") && i+1 < len(lines) {
+			i = i + 1
+			value = value[:len(value)-1] + lines[i]
+		}
+		value = strings.ReplaceAll(value, "\\n", "\n")
+		if err := p.Set(key, value); err != nil {
+			return errors.Wrapf(err, "properties 格式错误，位于 %d 行", i)
+		}
+	}
+	return nil
+}
