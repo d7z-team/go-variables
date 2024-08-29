ac7bd5dd590dcbd9238c8be7546241cdca7bc205
年幼涩批 <explodingfkl@gmail.com>
2024-08-29 15:07:44 +0800

    新增更多的测试

--- parser_yaml_test.go
+++ parser_yaml_test.go
@@ -1,6 +1,7 @@
 package variables
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -21,4 +22,19 @@ data:
 	s, err := variables.Template()("{{index .data 0 }}")
 	assert.NoError(t, err)
 	assert.Equal(t, "dragon", s)
+}
+
+func TestVariables_FromYamlFilter(t *testing.T) {
+	variables := NewVariables()
+	err := variables.FromYamlFilter(`
+metadata:
+  name: dragon
+  version: 1.0.0
+`, "", func(key string) bool {
+		return strings.HasPrefix(key, "metadata.")
+	})
+	assert.NoError(t, err)
+	s, err := variables.Template()("{{.metadata.name }}-{{.metadata.version}}")
+	assert.NoError(t, err)
+	assert.Equal(t, "dragon-1.0.0", s)
 }
