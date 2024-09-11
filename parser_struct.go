60d883fcd08270fe4674dfd5cc1fd1daa54298aa
dragon <ExplodingFKL@gmail.com>
2024-09-11 16:23:41 +0800

    增加 struct 的错误修复

--- parser_struct.go
+++ parser_struct.go
@@ -1,19 +1,40 @@
 package variables
 
-import "github.com/goccy/go-yaml"
+import (
+	"reflect"
+
+	"github.com/goccy/go-yaml"
+	"github.com/pkg/errors"
+)
+
+func toYaml(src any) (string, error) {
+	if src == nil {
+		return "", errors.New("对象为 nil")
+	}
+	v := reflect.ValueOf(src)
+	kind := v.Kind()
+	if kind >= reflect.Bool && kind <= reflect.Complex128 {
+		return "", errors.Errorf("对象为基础类型 %T", src)
+	}
+	marshal, err := yaml.Marshal(src)
+	if err != nil {
+		return "", nil
+	}
+	return string(marshal), nil
+}
 
 func (p *Variables) FromStruct(src any, namespace string) error {
-	marshal, err := yaml.Marshal(src)
+	s, err := toYaml(src)
 	if err != nil {
 		return err
 	}
-	return p.FromYaml(string(marshal), namespace)
+	return p.FromYaml(s, namespace)
 }
 
 func (p *Variables) FromStructFilter(src any, namespace string, filter func(key string) bool) error {
-	marshal, err := yaml.Marshal(src)
+	s, err := toYaml(src)
 	if err != nil {
 		return err
 	}
-	return p.FromStructFilter(string(marshal), namespace, filter)
+	return p.FromStructFilter(s, namespace, filter)
 }
